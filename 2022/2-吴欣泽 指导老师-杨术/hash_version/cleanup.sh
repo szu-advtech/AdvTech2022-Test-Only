7e043c5ae48c9b54b005726dae2c01cba7e8c8b4
shiftpop0 <79691045+shiftpop0@users.noreply.github.com>
2022-12-23 16:24:51 +0800

    Delete 2022 directory

--- 2022/2-吴欣泽 指导老师-杨术/hash_version/cleanup.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-rm -f *.pcap
-rm -f index.html*
--- 2022/2-吴欣泽 指导老师-杨术/hash_version/env.sh
+++
@@ -1,3 +0,0 @@
-BMV2_PATH=/home/myp4/Downloads/Anti-spoof/p4_environment/behavioral-model
-P4C_BM_PATH=/home/myp4/Downloads/Anti-spoof/p4_environment/p4c-bm
-
